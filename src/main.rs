6ca075182b2e87b75af0a228b6a9f368ccc6b6b3
Mitya Koval <dima.alex.koval@gmail.com>
2025-10-04 13:42:04 +0200

    Revert "handle multiple commands"
    This reverts commit 0acc192799779b84179ed52dfdd6516edc48cd69.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,18 @@
 #![allow(unused_imports)]
 
-use std::io::{Read, Write};
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    loop {
-        for stream in listener.incoming() {
-            match stream {
-                Ok(mut stream) => {
-                    let mut buf = [0; 512];
-                    loop {
-                        let command = stream.read(&mut buf).unwrap();
-
-                        if command == 0 {
-                            break;
-                        }
-
-                        stream.write_all(b"+PONG\r\n").unwrap()
-                    }
-                }
-                Err(e) => {
-                    println!("error: {}", e);
-                }
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+               stream.write_all(b"+PONG\r\n").unwrap()
+            }
+            Err(e) => {
+                println!("error: {}", e);
             }
         }
     }
