e3c2855650e65a3ddeb6b8260a2931f6347c14da
Mitya Koval <dima.alex.koval@gmail.com>
2025-10-04 13:42:59 +0200

    handle multiple command
    Signed-off-by: Mitya Koval <dima.alex.koval@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 #![allow(unused_imports)]
 
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-               stream.write_all(b"+PONG\r\n").unwrap()
-            }
-            Err(e) => {
-                println!("error: {}", e);
+    loop {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut stream) => {
+                    let mut buf = [0; 512];
+                    loop {
+                        let command = stream.read(&mut buf).unwrap();
+
+                        if command == 0 {
+                            break;
+                        }
+
+                        stream.write_all(b"+PONG\r\n").unwrap()
+                    }
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
         }
     }
