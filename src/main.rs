ae4ba6924e0b27711bd121b3afc2f3ac03912336
Mitya Koval <dima.alex.koval@gmail.com>
2025-10-06 13:42:25 +0200

    handle multiple concurrent connections
    Signed-off-by: Mitya Koval <dima.alex.koval@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,42 @@
 #![allow(unused_imports)]
 
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::net::{TcpListener, TcpStream};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    loop { // Event loop
-        for stream in listener.incoming() {
-            match stream {
-                Ok(mut stream) => {
-                    let mut buf = [0; 512];
-                    loop {
-                        let command = stream.read(&mut buf).unwrap();
-
-                        if command == 0 {
-                            break;
-                        }
-
-                        stream.write_all(b"+PONG\r\n").unwrap()
-                    }
-                }
-                Err(e) => {
-                    println!("error: {}", e);
-                }
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                // Reading a stream might block the thread
+                tokio::task::spawn_blocking(move || handle_connection(stream));
+            }
+            Err(e) => {
+                eprintln!("Error obtaining stream: {}", e)
             }
         }
     }
 }
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    loop {
+        match stream.read(&mut buf) {
+            Ok(size) => {
+                if size == 0 {
+                    break;
+                }
+
+                stream.write_all(b"+PONG\r\n").unwrap()
+            }
+            Err(e) => {
+                eprintln!("Error reading stream: {}", e)
+            }
+        }
+    }
+}
+
+// fn parse_command(string: String) -> Commands {
+// }
