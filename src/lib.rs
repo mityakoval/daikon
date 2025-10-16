2b042f16c91ede6356dacd875331e1fc014aa9a6
mitya <dima.alex.koval@gmail.com>
2025-10-17 00:21:01 +0200

    parser, which doesn't work yet
    Signed-off-by: mitya <dima.alex.koval@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,67 @@
-use crate::resp_parser::parser::parse;
+use std::fmt::Display;
+use crate::resp_parser::parser::parse_command;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
 mod resp_parser;
 
-enum Command {
+enum Command<'a> {
     PING,
-    ECHO(String),
+    ECHO(Value<'a>),
 }
 
-impl Command {
-    fn respond(&self, mut stream: &TcpStream) {
+impl Command<'_> {
+    fn respond(&mut self, mut stream: &TcpStream) {
         match self {
             Command::PING => stream.write_all(b"+PONG\r\n").unwrap(),
-            Command::ECHO(arg) => {
-                stream.write_all(arg.as_bytes()).unwrap()
-            },
+            Command::ECHO(arg) => stream.write_all(arg.encode().as_slice()).unwrap(),
         }
     }
 }
 
-enum RESPType {
-    SimpleString(String),
-    BulkString(String),
-    Error(String)
+trait RESPType {
+    fn encode(&mut self) -> Vec<u8>;
+    
+    fn len(&self) -> usize;
 }
 
-impl RESPType {
+#[derive(Debug)]
+enum Value<'a> {
+    Array(Vec<Value<'a>>),
+    SimpleString(&'a str),
+    BulkString(&'a str),
+    Error(&'a str),
+}
+
+impl<'a> RESPType for Value<'a> {
+    fn encode(&mut self) -> Vec<u8> {
+        match self {
+            Value::Array(array) => {
+                let mut encoded: Vec<u8> = Vec::new();
+                // Prepend with the array length
+                encoded.extend_from_slice(format!("*{}\r\n", array.len()).as_bytes());
+
+                array
+                    .into_iter()
+                    .flat_map(|t| t.encode())
+                    .for_each(|c| encoded.push(c));
+
+                encoded
+            }
+            Value::SimpleString(value) => value.as_bytes().to_vec(),
+            Value::BulkString(value) => value.as_bytes().to_vec(),
+            Value::Error(msg) => msg.as_bytes().to_vec(),
+        }
+    }
     
+    fn len(&self) -> usize {
+        match self {
+            Value::Array(values) => { values.len() }
+            Value::SimpleString(value) => { value.len() }
+            Value::BulkString(value) => { value.len() }
+            Value::Error(value) => { value.len() }
+        }
+    }
 }
 
 pub fn handle_connection(mut stream: &TcpStream) {
@@ -41,12 +75,10 @@ pub fn handle_connection(mut stream: &TcpStream) {
 
                 match str::from_utf8(&buf) {
                     Ok(input) => {
-                        let command = parse(input).unwrap();
+                        let mut command = parse_command(input).unwrap();
                         command.respond(&stream);
                     }
-                    Err(e) => {
-
-                    }
+                    Err(_e) => {}
                 }
             }
             Err(e) => {
--- src/resp_parser/parser.rs
+++ src/resp_parser/parser.rs
@@ -1,18 +1,71 @@
-use std::fmt::{Display, Formatter};
-use anyhow::{Error};
+use crate::{Command, RESPType, Value};
+use anyhow::Error;
+use std::fmt::{Debug, Display, Formatter};
 use thiserror::Error;
-use crate::Command;
 
-pub fn parse(input: &str) -> anyhow::Result<Command> {
-    if let Some(rest) = input.strip_prefix("*") {
+pub fn parse_command(input: &str) -> anyhow::Result<Command<'_ >> {
+    match parse_data(input).unwrap() {
+        (Value::Array(command_array), _) => {
+            eprintln!("command array: {:?}", command_array);
+            let mut command_array = command_array.into_iter();
+            match command_array.next().unwrap() {
+                Value::BulkString(command_bulk_str) => {
+                    eprintln!("command: {}", command_bulk_str);
+                    match command_bulk_str {
+                        "ECHO" => {
+                            let arg = command_array.next().unwrap();
 
+                            Ok(Command::ECHO(command_array.next().unwrap()))
+                        }
+                        "PING" => {
+                            Ok(Command::PING)
+                        }
+                        _ => { Err(Error::msg("Unknown command")) }
+                    }
+                }
+                _ => { Err(Error::msg("Command must be a RESP array")) }
+            }
+        },
+        _ => Err(Error::msg("Command must be a RESP array")),
     }
-    Err(Error::msg("Unexpected input"))
+}
+
+pub fn parse_data(input: &str) -> Option<(Value<'_>, &str)> {
+    if let Some((chunk, rest)) = input.split_once("\r\n") {
+        eprintln!("parsing chunk: {}, rest: {:?}", chunk, rest.trim());
+        let mut chars = chunk.chars();
+        match chars.next() {
+            Some('$') => {
+                // It's a BulkString
+                if let Some(length) = chars.next().unwrap().to_digit(10) {
+                    eprintln!("got bulk string of length: {}", length);
+                    Some((Value::BulkString(&rest[..(length as usize)]), rest))
+                } else {
+                    None
+                }
+            }
+            Some('*') => {
+                // It's a RESPArray
+                let length = chunk.chars().next().unwrap().len_utf8();
+                let mut array = Vec::with_capacity(length);
+                let mut rest = rest;
+                for i in 0..length {
+                    let (t, new_rest) = parse_data(rest)?;
+                    array.push(t);
+                    rest = new_rest;
+                }
+                Some((Value::Array(array), rest))
+            }
+            _ => None,
+        }
+    } else {
+        None
+    }
 }
 
 #[derive(Error, Debug)]
 enum ParseError {
-    UnexpectedInput()
+    UnexpectedInput(),
 }
 
 impl Display for ParseError {
